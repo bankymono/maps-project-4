bb43f2023655dd1ebc93660291cade7d0a5cfb51
bankymono <bankymono@gmail.com>
2020-06-21 10:00:47 +0100

    styling maps

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -5,7 +5,7 @@ class GoogleMaps extends Component {
         super(props)
         this.state={
         markers : [],
-        map: ''
+        map : ''
         }
         
         this.googleMapRef = createRef()
@@ -26,12 +26,81 @@ class GoogleMaps extends Component {
     }
 
     createGoogleMap = () => {
+        var styles = [
+            {
+              featureType: 'water',
+              stylers: [
+                { color: '#19a0d8' }
+              ]
+            },{
+              featureType: 'administrative',
+              elementType: 'labels.text.stroke',
+              stylers: [
+                { color: '#ffffff' },
+                { weight: 6 }
+              ]
+            },{
+              featureType: 'administrative',
+              elementType: 'labels.text.fill',
+              stylers: [
+                { color: '#e85113' }
+              ]
+            },{
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [
+                { color: '#efe9e4' },
+                { lightness: -40 }
+              ]
+            },{
+              featureType: 'transit.station',
+              stylers: [
+                { weight: 9 },
+                { hue: '#e85113' }
+              ]
+            },{
+              featureType: 'road.highway',
+              elementType: 'labels.icon',
+              stylers: [
+                { visibility: 'off' }
+              ]
+            },{
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [
+                { lightness: 100 }
+              ]
+            },{
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [
+                { lightness: -100 }
+              ]
+            },{
+              featureType: 'poi',
+              elementType: 'geometry',
+              stylers: [
+                { visibility: 'on' },
+                { color: '#f0e4d3' }
+              ]
+            },{
+              featureType: 'road.highway',
+              elementType: 'geometry.fill',
+              stylers: [
+                { color: '#efe9e4' },
+                { lightness: -25 }
+              ]
+            }
+          ]
+          
       const map  = new window.google.maps.Map(this.googleMapRef.current,{
             zoom :13,
+            styles : styles,
             center :{
                 lat:40.7413549,
                 lng:-73.9980244
-            }
+            },
+            mapTypeControl: false
         })
 
         this.setState({
@@ -50,6 +119,10 @@ class GoogleMaps extends Component {
 
         const largeInfoWindow = new window.google.maps.InfoWindow()
 
+        var defaultIcon = this.makeMarkerIcon('0091ff')
+
+        var highlightedIcon = this.makeMarkerIcon('FFFF24')
+
         for (var i = 0; i < locations.length; ++i){
             var position = locations[i].location
             var title = locations[i].title
@@ -57,21 +130,40 @@ class GoogleMaps extends Component {
             const marker = new window.google.maps.Marker({
                 position:position,
                 title:title,
+                icon:defaultIcon,
                 animation:window.google.maps.Animation.DROP,
                 id:i
             })
             this.setState(prevState=>{
-               return {markers:[...prevState.markers,marker]}
+               return {markers:[...prevState.markers, marker]}
             })
             window.populateInfoWindow = this.populateInfoWindow
             marker.addListener('click',function(){
                 window.populateInfoWindow(this,largeInfoWindow)
+            })
+            marker.addListener('mouseover', function(){
+              this.setIcon(highlightedIcon)
+            })
+            marker.addListener('mouseout', function(){
+              this.setIcon(defaultIcon)
             })
         }
         this.showListingsRef.current.addEventListener('click',this.showListings)
         this.hideListingsRef.current.addEventListener('click',this.hideListings)
     }
     
+    makeMarkerIcon = (markerColor) =>{
+      var markerImage = new window.google.maps.MarkerImage('http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new window.google.maps.Size(21,34),
+      new window.google.maps.Point(0,0),
+      new window.google.maps.Point(10,34),
+      new window.google.maps.Size(21,34)
+      )
+
+      return markerImage
+    }
+
     showListings = () =>{
         const bounds = new window.google.maps.LatLngBounds()
 
@@ -116,7 +208,7 @@ class GoogleMaps extends Component {
         return (
             <div>
                 <div className='container'>
-                    <div className="opptions-box">
+                    <div className="options-box">
                         <h5>Find Your New NYC Home</h5>
                         <div>
                             <input ref={this.showListingsRef} id="show-listings" type="button" value="Show Listings" />
